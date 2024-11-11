9c46a7d28c5c9f382b21ba3d88cad78f35be0263
NJWasTaken <114809171+NJWasTaken@users.noreply.github.com>
2024-11-11 14:25:02 +0530

    fixed hack.h

--- Hackathon/hack.h
+++ Hackathon/hack.h
@@ -19,12 +19,3 @@ typedef struct rescued{
     int rank;
     RESCUED *next;
 } RESCUED;
-
-ZONE *create_zone(int priority, int resources, int name);
-void add_person_to_zone(ZONE *zone, char *name);
-ZONE *get_high_priority_zone(ZONE *head);
-void distribute_supplies(ZONE *zone);
-bool rescue_person(ZONE *zone, char *name);
-void add_to_rescued_tree(RESCUED **root, PERSON *person, int rank);
-PERSON *search_rescued(RESCUED *root, char *name);
-void free_rescue_mission(ZONE *zones, RESCUED *rescued);
